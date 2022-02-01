7de5b51033c1bf6d7fa5ba08f98c7a260ff49515
59 <5.9.devv@gmail.com>
2022-02-01 23:25:09 +0900

    "init OddEvenGame"

---
+++ 2. OddEvenGame_udemy/OddEvenGame/OddEvenGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvenGame
+//
+//  Created by 59 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
