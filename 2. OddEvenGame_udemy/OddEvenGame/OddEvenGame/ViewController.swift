04302791714df177980bf0631dd6d2418c43208a
59 <5.9.devv@gmail.com>
2022-02-08 04:29:08 +0900

    배팅 기능 개선

--- 2. OddEvenGame_udemy/OddEvenGame/OddEvenGame/ViewController.swift
+++ 2. OddEvenGame_udemy/OddEvenGame/OddEvenGame/ViewController.swift
@@ -39,8 +39,7 @@ class ViewController: UIViewController {
         // 시작할 때는, fist image가 등장하면 안되므로 숨김처리
         self.imageContainer.isHidden = true
     }
-
-    
+      
     // 게임시작 버튼을 눌렀을 때 실행될 것들
     @IBAction func gameStartPressed(_ sender: Any) {
         print("게임시작!!")
@@ -63,7 +62,7 @@ class ViewController: UIViewController {
     
     // alert을 보여주는 함수
     func showAlert() {
-        // alert 창 생성
+        // alert 생성
         let alert = UIAlertController.init(title: "Game Start!", message: "홀 짝을 선택해주세요.", preferredStyle: .alert)
         // 홀 버튼
         let oddBtn = UIAlertAction.init(title: "홀", style: .default) { _ in
@@ -73,12 +72,18 @@ class ViewController: UIViewController {
             // guard를 통해 없을 경우에는 아무것도 하지 않게 한다.
             // guard 문은 두 개를 동시에 쓸 수도 있다.
             // value를 Int 타입으로 변형하기 위한 guard 문을 동시에 사용했다.
-            guard let input = alert.textFields?.first?.text, let value = Int(input) else {
-                return
-            }
+            let input: String = alert.textFields?.first?.text ?? "0"
+            let value: Int = Int(input) ?? 0
             
             print("입력된 값은 \(input) 입니다.")
-            self.getWinner(betBallCount: value, userChoice: "홀")
+            
+            if value != 0 && self.userBallsCount >= value {
+                self.getWinner(betBallCount: value, userChoice: "홀")
+            } else {
+                self.warningAlert(betBallCount: value,
+                                  currentBallCount: self.userBallsCount)
+            }
+            
             
         }
         // 짝 버튼
@@ -96,7 +101,13 @@ class ViewController: UIViewController {
             }
             
             print("입력된 값은 \(input) 입니다.")
-            self.getWinner(betBallCount: value, userChoice: "짝")
+            
+            if value != 0 && self.userBallsCount >= value {
+                self.getWinner(betBallCount: value, userChoice: "짝")
+            } else {
+                self.warningAlert(betBallCount: value,
+                                  currentBallCount: self.userBallsCount)
+            }
         }
         
         // alert 창에 버튼 및 입력창을 추가해준다.
@@ -111,6 +122,26 @@ class ViewController: UIViewController {
         self.present(alert, animated: true) {
             print("화면이 띄워졌습니다.")
         }
+    }
+    
+    // 현재 가진 구슬보다 많이 배팅할 수 없고
+    // 0개를 배팅할 수 없도록 alert 생성
+    func warningAlert(betBallCount: Int, currentBallCount: Int) {
+        var warningMsg: String = ""
+        if betBallCount == 0 {
+            warningMsg = "1개 이상 배팅해주세요."
+        } else if currentBallCount < betBallCount {
+            warningMsg = "구슬이 부족합니다."
+        }
+        let warningAlert = UIAlertController.init(title: "앗!", message: warningMsg, preferredStyle: .alert)
+        
+        let confirmBtn = UIAlertAction.init(title: "다시 배팅하기", style: .default) {
+            _ in self.showAlert()
+        }
+        
+        warningAlert.addAction(confirmBtn)
+    
+        self.present(warningAlert, animated: true)
     }
 
     // 매 경기마다의 승자 판별하는 함수
@@ -136,8 +167,8 @@ class ViewController: UIViewController {
     func calculateBalls(winner: String, betBallCount: Int) -> Void {
         if winner == "USER" {
             if checkPocketEmpty(balls: self.comBallsCount - betBallCount) {
-                self.userBallsCount += comBallsCount
-                self.comBallsCount = 0
+                self.userBallsCount = 20
+                self.comBallsCount = 20
                 self.resultLbl.text = "유저 최종 승리!"
             } else {
                 self.comBallsCount -= betBallCount
@@ -146,8 +177,8 @@ class ViewController: UIViewController {
             
         } else {
             if checkPocketEmpty(balls: self.userBallsCount - betBallCount) {
-                self.comBallsCount += userBallsCount
-                self.userBallsCount = 0
+                self.comBallsCount = 20
+                self.userBallsCount = 20
                 self.resultLbl.text = "컴퓨터 최종 승리!"
             } else {
                 self.userBallsCount -= betBallCount
