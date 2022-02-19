5085ee0a000de8818e57a2cd50d0e9a6fdd28300
59 <5.9.devv@gmail.com>
2022-02-20 03:15:38 +0900

    Snapkit 실습 프로젝트 생성 및 pod init

---
+++ 2.1.OddEvenGame_Snapkit/OddEvenGame_Snapkit/OddEvenGame_Snapkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvenGame_Snapkit
+//
+//  Created by 59 on 2022/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
