16a542dba3f6af73887b53473839b83db247dd4e
59 <5.9.devv@gmail.com>
2022-02-20 18:35:49 +0900

    Navigation Controller 생성 및 연결, UI 객체 생성

--- 2.1.OddEvenGame_Snapkit/OddEvenGame_Snapkit/OddEvenGame_Snapkit/AppDelegate.swift
+++ 2.1.OddEvenGame_Snapkit/OddEvenGame_Snapkit/OddEvenGame_Snapkit/AppDelegate.swift
@@ -16,11 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // UIWindow 객체를 생성한다.
         window = UIWindow(frame: UIScreen.main.bounds)
+        // View Controller와, 그 앞에 있는 Navigation Controller를 만들어준다.
+        let mainVC = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        // 윈도우 위에, 실제로 보여질 View를 ViewController로 설정해준다.
+        window?.rootViewController = navigationController
         // 만든 윈도우를 'Key'로 만들고, 보여지도록 설정한다.
         window?.makeKeyAndVisible()
-        // 윈도우 위에, 실제로 보여질 View를 ViewController로 설정해준다.
-        window?.rootViewController = ViewController()
-    
+
         return true
     }
 }
--- 2.1.OddEvenGame_Snapkit/OddEvenGame_Snapkit/OddEvenGame_Snapkit/ViewController.swift
+++ 2.1.OddEvenGame_Snapkit/OddEvenGame_Snapkit/OddEvenGame_Snapkit/ViewController.swift
@@ -5,16 +5,47 @@
 //  Created by 59 on 2022/02/20.
 //
 
+import AVFoundation
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    // 컴포넌트 객체 생성
+    private var computerBallCountLbl: UILabel = UILabel()
+    private var userBallCountLbl: UILabel = UILabel()
+    private var resultLbl: UILabel = UILabel()
+    private var imageContainer: UIView = UIView()
+    private var fistImage: UIImageView = UIImageView()
+    private var gameStartBtn: UIButton = UIButton()
+    private var settingBtn: UIButton = UIButton()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        // view 배경 색상
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        // 위에서 만들어준 객체 리스트
+        let subViewList: [UIView] = [computerBallCountLbl, userBallCountLbl, resultLbl
+                           , imageContainer, fistImage, gameStartBtn, settingBtn]
+        // 리스트를 돌면서, 모두 subView에 띄우기
+        for subView in subViewList {
+            view.addSubview(subView)
+        }
+        
+        // 각 객체의 레이아웃 설정
+        
+        /* 1. computerBallCountLbl, userBallCountLbl : 공 갯수 라벨
+         * (1) 텍스트 폰트, 크기, 색상 설정
+         * (2) 제약 설정
+         */
+        
+        
+        
         
     }
-
-
 }
 
